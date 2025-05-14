3f6853e2363af5519c9427163a245bbaf208db3d
Miguel-Araya <miguelarayaarroyo12345@gmail.com>
2025-05-13 18:13:56 -0600

    Validacion de la navegacion del admin

--- src/app/auth/pages/edit-page/edit-page.component.ts
+++ src/app/auth/pages/edit-page/edit-page.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+interface TokenResponse {
+  tokenDTOString: string;
+}
+
 @Component({
   selector: 'app-edit-page',
   standalone: true,
@@ -8,5 +14,39 @@ import { CommonModule } from '@angular/common';
   styleUrl: './edit-page.component.css'
 })
 export default class EditPageComponent {
+
+  isLoading = signal(true);
+  constructor(private router: Router) {
+    
+  }
+  login= inject(LoginService);
+
+    ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    console.log('Token:', token);
+    
+    if (token) {
+      this.login.verifyToken(token).subscribe({
+        next: (tokenResponse) => {
+         
+          this.isLoading.set(false);
+          if(!tokenResponse) {
+           
+           
+            this.router.navigate(['/login']);
+          
+          }
+      
+        },
+        error: (err) => {
+          console.error('Error loading pages', err);
+        }
+      });
+    } else {
+      console.error('No token found');
+      // Redirect to login page
+      this.router.navigate(['/login']);
+    }
+  }
 
 }
