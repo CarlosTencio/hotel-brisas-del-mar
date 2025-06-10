cbad56caeff6a29a9ac986d3a4e7017b56f880be
EdwinLF <117051836+EdwinLF@users.noreply.github.com>
2025-06-09 19:40:24 -0600

    US12 Terminado
    tiempo estimado: 10 horas
    
    tiempo real: 12 horas

--- src/app/auth/pages/edit-page/edit-page.component.ts
+++ src/app/auth/pages/edit-page/edit-page.component.ts
@@ -23,31 +23,30 @@ export default class EditPageComponent {
 
     ngOnInit(): void {
     const token = localStorage.getItem('token');
-    // console.log('Token:', token);
+    console.log('Token:', token);
     
-    // if (token) {
-    //   this.login.verifyToken(token).subscribe({
-    //     next: (tokenResponse) => {
+    if (token) {
+      this.login.verifyToken(token).subscribe({
+        next: (tokenResponse) => {
          
-    //       this.isLoading.set(false);
-    //       if(!tokenResponse) {
+          this.isLoading.set(false);
+          if(!tokenResponse) {
            
            
-    //         this.router.navigate(['/login']);
+            this.router.navigate(['/login']);
           
-    //       }
+          }
       
-    //     },
-    //     error: (err) => {
-    //       console.error('Error loading pages', err);
-    //     }
-    //   });
-    // } else {
-    //   console.error('No token found');
-    //   // Redirect to login page
-    //   this.router.navigate(['/login']);
-    // }
-    this.isLoading = signal(false);
+        },
+        error: (err) => {
+          console.error('Error loading pages', err);
+        }
+      });
+    } else {
+      console.error('No token found');
+      // Redirect to login page
+      this.router.navigate(['/login']);
+    }
   }
 
 }
