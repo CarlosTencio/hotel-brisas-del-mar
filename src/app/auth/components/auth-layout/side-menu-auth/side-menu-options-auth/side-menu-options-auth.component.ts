156ccb5a8556a4b7e4fbcdba93712a49a583e62a
Miguel-Araya <miguelarayaarroyo12345@gmail.com>
2025-06-13 11:21:12 -0600

    Quitar opcion de modificar principal

--- src/app/auth/components/auth-layout/side-menu-auth/side-menu-options-auth/side-menu-options-auth.component.ts
+++ src/app/auth/components/auth-layout/side-menu-auth/side-menu-options-auth/side-menu-options-auth.component.ts
@@ -30,10 +30,6 @@ export  class SideMenuOptionsAuthComponent {
       route: '/home-auth/edit-page'
     },
     {
-      label: 'Editar página principal',
-      route: '/home-auth/edit-home'
-    },
-    {
       label: 'Listado de reservaciones',
       route: '/home-auth/list-booking'
     },
