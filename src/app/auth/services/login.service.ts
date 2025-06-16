6f1e2f2070173d65f100c10c9e913e6b8e4539d2
Miguel-Araya <miguelarayaarroyo12345@gmail.com>
2025-06-16 10:38:46 -0600

    Cambio de Base de datos y cambio de servidor en azure

--- src/app/auth/services/login.service.ts
+++ src/app/auth/services/login.service.ts
@@ -13,7 +13,8 @@ interface TokenResponse {
 
 export class LoginService {
   constructor(private http: HttpClient) { }
-  private loginURL = 'https://hotelauth-hrbdbne0c3b3gagy.canadacentral-01.azurewebsites.net/api/Admin/AuthWithCredentials';
+  private baseURL = 'https://apihotel-emhmc5e6hpdfgwfg.canadacentral-01.azurewebsites.net';
+  private loginURL = `${this.baseURL}/api/Admin/AuthWithCredentials`;
   login(userNameDTO: string, passwordDTO: string): Observable<TokenResponse> {
     const body = { userNameDTO, passwordDTO };
     
@@ -23,7 +24,7 @@ export class LoginService {
       })
     );
   }
-  private tokenURL = 'https://hotelauth-hrbdbne0c3b3gagy.canadacentral-01.azurewebsites.net/api/Admin/VerifyToken';
+  private tokenURL = `${this.baseURL}/api/Admin/VerifyToken`;
   verifyToken(tokenDTOString: string): Observable<TokenResponse[]> {
     const body = { tokenDTOString};
     return this.http.post<TokenResponse[]>(this.tokenURL,body ).pipe(
